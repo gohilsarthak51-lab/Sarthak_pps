3a86ea74b21d088cc13070cb703a496cc645818e
Sarthak Gohil <gohilsarthak51@gmail.com>
2026-02-07 17:45:11 +0530

    Add files via upload

---
+++ p1.c
@@ -0,0 +1,20 @@
+# include<stdio.h>
+  int main()
+{
+
+    int a = 100;
+    int b = 50;
+    
+    printf("Addition is a+b =%d\n", a+b);
+        printf("Subtraction is a-b =%d\n", a-b);
+                printf("Multiplication is a*b =%d\n", a*b);
+                      printf("Division is a/b =%d\n", a/b);
+                              printf("Modulus is = %d\n", a%b);
+
+
+        
+
+
+
+     return 0;
+}
