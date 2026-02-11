c76c278cc6d7a2c5c0dccc7087fd6f22ac19ec05
Sarthak Gohil <gohilsarthak51@gmail.com>
2026-02-11 08:54:17 +0530

    Update p1.c
    Updated the program to handle floating-point numbers and modified the output format for calculations.

--- p1.c
+++ p1.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
-int main()
- {
-    int a, b;
+int main() {
+    float a, b;
 
-    printf("Enter the value of a and b ");
-    scanf("%d %d", &a, &b);
+    printf("Enter the value of a and b: ");
+    scanf("%f %f", &a, &b);
 
-    printf("Addition = %d\n", a + b);
+    printf("Addition = %.2f\n", a + b);
+    printf("Subtraction = %.2f\n", a - b);
+    printf("Multiplication = %.2f\n", a * b);
+    printf("Division = %.2f\n", a / b);
 
-    printf("Subtraction = %d\n", a - b);
-
-    printf("Multiplication = %d\n", a * b);
-    printf("Division = %d\n", a / b);
-
-    printf("Modulus = %f\n", (float)(a % b));
+    // Type casting for modulus
+    printf("Modulus = %d\n", (int)a % (int)b);
 
     return 0;
-
 }
+
+/* output value
+Enter the value of a and b: 100   20
+ Addition = 120.000000
+ Subtraction = 80.000000
+ Multiplication = 2000.000000
+ Division = 5.000000
+ Modulus = 0  */
