43ffe37b89b609b28b17282ec00dc5db97b6bc3c
Sarthak Gohil <gohilsarthak51@gmail.com>
2026-02-22 08:03:51 +0530

    Update p2-p2.c
    Updated the program to use loops for input validation and improved output formatting.

--- p2-p2.c
+++ p2-p2.c
@@ -1,76 +1,66 @@
 #include <stdio.h>
 
-int main() {
+int main() 
+{
     int m1, m2, m3, m4, m5;
     int total;
     float percentage;
 
+    printf("\n------ STUDENT MARKSHEET ------\n");
     printf("Enter marks out of 100\n");
 
-    printf("English: ");
-    scanf("%d", &m1);
-    if (m1 > 100 || m1 < 0) {
-        printf("Invalid marks in English!\n");
-        return 0;
-    }
+    do {
+        printf("English: ");
+        scanf("%d", &m1);
+        if (m1 < 0 || m1 > 100)
+            printf("Please enter marks between 0 and 100.\n");
+    } while (m1 < 0 || m1 > 100);
 
-    printf("Maths: ");
-    scanf("%d", &m2);
-    if (m2 > 100 || m2 < 0) {
-        printf("Invalid marks in Maths!\n");
-        return 0;
-    }
+    do {
+        printf("Maths: ");
+        scanf("%d", &m2);
+        if (m2 < 0 || m2 > 100)
+            printf("Please enter marks between 0 and 100.\n");
+    } while (m2 < 0 || m2 > 100);
 
-    printf("G.K.: ");
-    scanf("%d", &m3);
-    if (m3 > 100 || m3 < 0) {
-        printf("Invalid marks in G.K.!\n");
-        return 0;
-    }
+    do {
+        printf("G.K.: ");
+        scanf("%d", &m3);
+        if (m3 < 0 || m3 > 100)
+            printf("Please enter marks between 0 and 100.\n");
+    } while (m3 < 0 || m3 > 100);
 
-    printf("S.S.T.: ");
-    scanf("%d", &m4);
-    if (m4 > 100 || m4 < 0) {
-        printf("Invalid marks in S.S.T.!\n");
-        return 0;
-    }
+    do {
+        printf("S.S.T.: ");
+        scanf("%d", &m4);
+        if (m4 < 0 || m4 > 100)
+            printf("Please enter marks between 0 and 100.\n");
+    } while (m4 < 0 || m4 > 100);
 
-    printf("Science: ");
-    scanf("%d", &m5);
-    if (m5 > 100 || m5 < 0) {
-        printf("Invalid marks in Science!\n");
-        return 0;
-    }
+    do {
+        printf("Science: ");
+        scanf("%d", &m5);
+        if (m5 < 0 || m5 > 100)
+            printf("Please enter marks between 0 and 100.\n");
+    } while (m5 < 0 || m5 > 100);
 
     total = m1 + m2 + m3 + m4 + m5;
     percentage = total / 5.0;
 
-    printf("\n------ STUDENT MARKSHEET ------\n");
-    printf("Marks of English: %d\n", m1);
-    printf("Marks of Maths: %d\n", m2);
-    printf("Marks of G.K.: %d\n", m3);
-    printf("Marks of S.S.T.: %d\n", m4);
-    printf("Marks of Science: %d\n", m5);
-
-    printf("-------------------------------\n");
+    printf("\n------ RESULT ------\n");
     printf("Total Marks: %d\n", total);
-    printf("Percentage: %f\n", percentage);
+    printf("Percentage: %.2f\n", percentage);
 
-    if (percentage >= 90) {
-        printf("Grade: A\n");
-    }
-    else if (percentage >= 75) {
-        printf("Grade: B\n");
-    }
-    else if (percentage >= 60) {
-        printf("Grade: C\n");
-    }
-    else if (percentage >= 40) {
-        printf("Grade: D\n");
-    }
-    else {
-        printf("Result: Fail\n");
-    }
+    if (percentage >= 90)
+        printf("Grade A\n");
+    else if (percentage >= 75)
+        printf("Grade B\n");
+    else if (percentage >= 60)
+        printf("Grade C\n");
+    else if (percentage >= 40)
+        printf("Grade D\n");
+    else
+        printf("Fail\n");
 
     return 0;
 }
